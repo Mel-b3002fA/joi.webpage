b61b8f68f470462f39709aa40057ce3a7c068362
Milena <142824252+Mel-b3002fA@users.noreply.github.com>
2025-03-15 12:34:17 +0100

    push

--- nn/main.py
+++ nn/main.py
@@ -1,6 +1,6 @@
-inputs = [1.2, 5.1, 3.4]
+inputs = [1, 2, 3, 2.5]
 weights = [3.2, 4.4, 1.3]
 bias = 3
 
-output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+output = inputs[0]*weights[0] + inputs[1]*weights[1]  + inputs[2]*weights[2] + inputs[3]*weights[3] + bias
 print[output]
