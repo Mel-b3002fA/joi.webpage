3e3195baa70a9f19e451504a08fab1b75b2993d4
Milena <142824252+Mel-b3002fA@users.noreply.github.com>
2025-03-19 23:05:25 +0100

    push

--- nn/main.py
+++ nn/main.py
@@ -2,7 +2,7 @@ import numpy as np
 inputs = [1, 2, 3, 2.5]
 weights = [0.2, 0.8, -0.5, 1.0]
 bias = 2.0
-outputs = np.dot(weights, inputs) + bias
+output = np.dot(weights, inputs) + bias
 print(output)
 
 """ weights = [[0.2, 0.8, -0.5, 1,0],
