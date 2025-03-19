88b5b7390941a9e639a9d4e617aca142640e8687
Milena <142824252+Mel-b3002fA@users.noreply.github.com>
2025-03-19 23:04:50 +0100

    push

--- nn/main.py
+++ nn/main.py
@@ -3,6 +3,8 @@ inputs = [1, 2, 3, 2.5]
 weights = [0.2, 0.8, -0.5, 1.0]
 bias = 2.0
 outputs = np.dot(weights, inputs) + bias
+print(output)
+
 """ weights = [[0.2, 0.8, -0.5, 1,0],
            [0.5, -0.91, 0.26, -0.5],
            [-0.26, -0.27, 0.17, 0.87]]
